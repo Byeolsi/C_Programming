6fc64fde5b545118666d4503659cbf6bce58b1a4
Byeolsi <86233884+Byeolsi@users.noreply.github.com>
2021-06-23 17:00:21 +0900

    Add files via upload

---
+++ CS HW2 솔루션/CS HW2/Classes.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CS_HW2
+{
+    // DeliveryVehicle 클래스
+    class DeliveryVehicle
+    {
+        public int vehicleId { get; set; }
+        public string destination { get; set; }
+        public int priority { get; set; }
+
+        // DeliveryVehicle 생성자
+        // 필드를 모두 초기화.
+        public DeliveryVehicle(int vehicleId, string address, int priority)
+        {
+            this.vehicleId = vehicleId;
+            destination = address;
+            this.priority = priority;
+        }
+    }
+
+    // DeliveryVehicleManager 클래스
+    class DeliveryVehicleManager
+    {
+        int numWaitingPlaces;
+        ArrayList[] vehicleList;
+
+        // DeliveryVehicleManager 생성자
+        // 대기 장소 개수 멤버(numWaitingPlaces)를 초기화.
+        // vehicleList 의 인스턴스를 생성.
+        public DeliveryVehicleManager(int numWaitingPlaces)
+        {
+            this.numWaitingPlaces = numWaitingPlaces;
+            vehicleList = new ArrayList[numWaitingPlaces];
+
+            // vehicleList 배열의 요소마다 ArrayList 인스턴스를 생성.
+            for (int i = 0; i < numWaitingPlaces; i++)
+            {
+                vehicleList[i] = new ArrayList();
+            }
+        }
+
+        // checkVehicle 메소드
+        // vehicleId 를 확인하여 vehicleList 에 존재하는 자동차인지 확인.
+        // 있다면, true 를 반환. 없다면, false 를 반환.
+        private bool checkVehicle(int vehicleId)
+        {
+            for (int i = 0; i < numWaitingPlaces; i++)
+            {
+                foreach (DeliveryVehicle objVehicle in vehicleList[i])
+                {
+                    if (vehicleId == objVehicle.vehicleId)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        // Readyin 메소드
+        // vehicleList[i] 에 요소(DeliveryVehicle)을 삽입.
+        public void Readyin(int WaitingPlaceId, int vehicleId, string address, int priority)
+        {
+            int index;
+            
+            // 존재하는 자동차 번호인지 확인.
+            if (checkVehicle(vehicleId))
+            {
+                Console.WriteLine("이미 존재하는 자동차 번호입니다!");
+
+                return;
+            }
+
+            // 우선 순위에 맞게 vehicleList[i] 중간에 삽입.
+            foreach (DeliveryVehicle compareVehicle in vehicleList[WaitingPlaceId - 1])
+            {
+                if (priority < compareVehicle.priority)
+                {
+                    index = vehicleList[WaitingPlaceId - 1].IndexOf(compareVehicle);
+                    vehicleList[WaitingPlaceId - 1].Insert(index, new DeliveryVehicle(vehicleId, address, priority));
+
+                    // 삽입 후, 메소드 종료.
+                    return;
+                }
+            }
+
+            // vehicleList[i] 마지막에 삽입.
+            vehicleList[WaitingPlaceId - 1].Add(new DeliveryVehicle(vehicleId, address, priority));
+        }
+
+        // Ready 메소드
+        // vehicleLIst[i] 중에 가장 값의 개수가 적은 'i' 를 찾는다.
+        // Readyin() 메소드를 호출할 때, 'i + 1' 을 인자로 넘긴다.
+        public int Ready(int vehicleId, string address, int priority)
+        {
+            int index = 0, min = vehicleList[0].Count;
+
+            for (int i = 1; i < numWaitingPlaces; i++)
+            {
+                if (min > vehicleList[i].Count)
+                {
+                    index = i;
+                    min = vehicleList[i].Count;
+                }
+            }
+
+            Readyin(index + 1, vehicleId, address, priority);
+
+            return index + 1;
+        }
+
+        // Status 메소드
+        // 현재 상태를 출력.
+        public void Status(StreamWriter sw)
+        {
+            sw.WriteLine("************************* Delivery Vehicle Info *************************");
+            sw.WriteLine($"Number of WaitPlaces: {numWaitingPlaces}");
+            for (int i = 0; i < numWaitingPlaces; i++)
+            {
+                sw.WriteLine($"WaitPlace #{i + 1} Number Vehicles: {vehicleList[i].Count}");
+                foreach (DeliveryVehicle objVehicle in vehicleList[i])
+                {
+                    sw.WriteLine($"FNUM: {objVehicle.vehicleId} DEST: {objVehicle.destination} PRIO: {objVehicle.priority}");
+                }
+                sw.WriteLine("--------------------------------------------------");
+            }
+            sw.WriteLine("*********************** End Delivery Vehicle Info ***********************");
+        }
+
+        // Cancel 메소드
+        // vehicleId 가 일치하는 자동차의 index 를 vehicleList 에서 찾고, 이를 이용하여 삭제.
+        public bool Cancel(int vehicleId)
+        {
+            int index;
+
+            for (int i = 0; i < numWaitingPlaces; i++)
+            {
+                foreach (DeliveryVehicle objVehicle in vehicleList[i])
+                {
+                    if (vehicleId == objVehicle.vehicleId)
+                    {
+                        index = vehicleList[i].IndexOf(objVehicle);
+                        vehicleList[i].RemoveAt(index);
+
+                        return true;
+                    }
+                }
+            }
+
+            Console.WriteLine("존재하지 않는 자동차 번호입니다!");
+
+            return false;
+        }
+
+        // Deliver 메소드
+        // 입력 받은 번호의 대기 장소에서 vehicleList[i] 맨 앞에 있는 요소를 제거.
+        public int Deliver(int WaitingPlaceId)
+        {
+            int vehicleId;
+
+            DeliveryVehicle delVehicle;
+
+            if (vehicleList[WaitingPlaceId - 1].Count == 0)
+            {
+                Console.WriteLine("대기 장소에 자동차가 존재하지 않습니다!");
+
+                return -1;
+            }
+
+            delVehicle = vehicleList[WaitingPlaceId - 1][0] as DeliveryVehicle;
+            if (delVehicle == null)
+                Console.WriteLine("delVehicle, 형변환 실패!");
+
+            vehicleId = delVehicle.vehicleId;
+
+            vehicleList[WaitingPlaceId - 1].RemoveAt(0);
+
+            return vehicleId;
+        }
+
+        // Clear 메소드
+        // vehicleList[i] 의 요소들을 모두 제거.
+        public void Clear(int WaitingPlaceId)
+        {
+            vehicleList[WaitingPlaceId - 1].Clear();
+        }
+    }
+}
---
+++ CS HW2 솔루션/CS HW2/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.IO;
+
+namespace CS_HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int placeId, vehicleId, priority, numWaitingPlaces;
+            string tmpLine, order, address;
+            string[] splitLine = new string[5];
+
+            DeliveryVehicleManager VM;
+
+            StreamWriter sw = new StreamWriter(new FileStream("OUTPUT.txt", FileMode.Create));
+            StreamReader sr = new StreamReader(new FileStream("INPUT.txt", FileMode.Open));
+
+            tmpLine = sr.ReadLine();
+            // 잘못된 파일인지 확인.
+            if (tmpLine != null)
+            {
+                if ('0' <= tmpLine[0] && tmpLine[0] <= '9')
+                {
+                    numWaitingPlaces = int.Parse(tmpLine);
+
+                    VM = new DeliveryVehicleManager(numWaitingPlaces);
+                }
+                else
+                {
+                    Console.WriteLine("잘못된 입력 파일입니다!");
+
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("잘못된 입력 파일입니다!");
+
+                return;
+            }
+
+            // 명령어로 "Quit" 이 입력될 때까지 반복.
+            do
+            {
+                // 더 이상 읽어 들일 문자가 없는 경우, break.
+                if (sr.Peek() < 0)
+                {
+                    Console.WriteLine("파일에 더 이상 데이터가 존재하지 않습니다.");
+
+                    break;
+                }
+
+                tmpLine = sr.ReadLine();
+                splitLine = tmpLine.Split(" ", StringSplitOptions.None);
+
+                order = splitLine[0];
+
+                // 명령어에 따라 다르게 수행.
+                switch (order)
+                {
+                    case "ReadyIn":
+                        // 앞에 문자를 제거한 후에 int 형으로 변환.
+                        placeId = int.Parse(splitLine[1].Remove(0, 1));
+                        vehicleId = int.Parse(splitLine[2]);
+                        address = splitLine[3];
+                        priority = int.Parse(splitLine[4].Remove(0, 1));
+
+                        VM.Readyin(placeId, vehicleId, address, priority);
+
+                        sw.WriteLine($"Vehicle {vehicleId} assigned to WaitPlace #{placeId}.");
+                        
+                        break;
+
+                    case "Ready":
+                        vehicleId = int.Parse(splitLine[1]);
+                        address = splitLine[2];
+                        priority = int.Parse(splitLine[3].Remove(0, 1));
+
+                        // Ready 메소드의 반환 값으로 대기 장소 ID 를 반환.
+                        placeId = VM.Ready(vehicleId, address, priority);
+
+                        // 반환 받은 값을 이용하여 파일에 쓴다.
+                        sw.WriteLine($"Vehicle {vehicleId} assigned to WaitPlace #{placeId}.");
+
+                        break;
+
+                    case "Status":
+                        VM.Status(sw);
+
+                        break;
+
+                    case "Cancel":
+                        vehicleId = int.Parse(splitLine[1]);
+
+                        VM.Cancel(vehicleId);
+
+                        sw.WriteLine($"Cancelation of Vehicle {vehicleId} completed.");
+
+                        break;
+
+                    case "Deliver":
+                        placeId = int.Parse(splitLine[1].Remove(0, 1));
+
+                        vehicleId = VM.Deliver(placeId);
+
+                        sw.WriteLine($"Vehicle {vehicleId} used to deliver.");
+
+                        break;
+
+                    case "Clear":
+                        placeId = int.Parse(splitLine[1].Remove(0, 1));
+
+                        VM.Clear(placeId);
+
+                        sw.WriteLine($"WaitPlace #{placeId} cleared.");
+
+                        break;
+
+                    default:
+                        if (order != "Quit")
+                            Console.WriteLine("인식할 수 없는 명령어입니다.");
+
+                        break;
+
+                }
+
+            } while (order != "Quit");
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
